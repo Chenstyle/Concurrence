6abb673ef47d1c4a3153fbf6d98cdd2555540dc1
Chenstyle <1789708722@qq.com>
2016-11-21 15:21:03 +0800

    replenish arithmetic

--- src/work/test/arithmetic/AboutArr.java
+++ src/work/test/arithmetic/AboutArr.java
@@ -7,7 +7,12 @@ import java.util.Random;
  *         2016/11/21 0021
  */
 public class AboutArr {
-
+    /**
+     * 初始化数组
+     *
+     * @param size 数组大小
+     * @return 初始后的数组
+     */
     public int[] initArr(int size) {
         int[] arr = new int[size];
         for (int i = 0; i < size; i++) {
@@ -16,6 +21,11 @@ public class AboutArr {
         return arr;
     }
 
+    /**
+     * 打印数组在控制台
+     *
+     * @param arr 需要打印的数组
+     */
     public void printArr(int[] arr) {
         for (int i : arr) {
             System.out.print(i + " ");
@@ -23,6 +33,12 @@ public class AboutArr {
         System.out.println("\n");
     }
 
+    /**
+     * 打乱一个数组中内容的顺序
+     *
+     * @param arr 要被打乱的数组
+     * @return 顺序打乱后的数组
+     */
     public int[] exchangeArr(int[] arr) {
         Random random = new Random();
         for (int i = 0; i < arr.length; i++) {
@@ -36,7 +52,8 @@ public class AboutArr {
     }
 
     /**
-     *  对 100 个数字进行排序
+     * 对 100 个数字进行排序
+     *
      * @param arr2 需要排序的对象
      * @return 排序完成的序列
      */
